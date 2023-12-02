ad9efc041da99032de2aeba7a2332228900bb0ec
romatorres <romatorres12@gmail.com>
2023-12-01 21:43:27 -0300

    Atualização casa 2

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -6,10 +6,10 @@ const UserSchema = new Schema(
     password: {
       type: String,
       required: true,
-      validate: pass => {
+      validate: (pass) => {
         if (!pass?.length || pass.length < 5) {
           new Error("A senha tem que ter no minimo 5 caracteres");
-          return false
+          return false;
         }
       },
     },
@@ -17,4 +17,10 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
-export const User = models?.User || model('User', UserSchema);
+UserSchema.post("validate", function (user) {
+  const notHashedPassword = user.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(notHashedPassword, salt);
+});
+
+export const User = models?.User || model("User", UserSchema);
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,32 +1,70 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function RegisterPage() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  function handleFormSubmit(ev) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+  async function handleFormSubmit(ev) {
     ev.preventDefault();
-    fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {'Content-Type': 'appliclation/json'},
-    });
+    setCreatingUser(true);
+    try {
+      await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "appliclation/json" },
+      });
+      setCreatingUser(false);
+      setUserCreated(true);
+    } catch (e) {
+      setError(true);
+    }
   }
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center text-gray-700">
+          Usuário criado, agora você pode fazer o{" "}
+          <Link href={"/login"}>
+            <strong>Login! &raquo;</strong>
+          </Link>
+        </div>
+      )}
+      {setError && (
+        <div className="my-4 text-center text-gray-700">
+          Algo deu errado, por favor, tente novamente!!
+        </div>
+      )}
       <form className="block max-w-md mx-auto" onSubmit={handleFormSubmit}>
-        <input type="email" placeholder="e-mail" value={email} onChange={ev => setEmail(ev.target.value)}/>
-        <input type="password" placeholder="senha" value={password} onChange={ev => setPassword(ev.target.value)} />
-        <button type="submit">Registre-se</button>
+        <input
+          type="email"
+          placeholder="e-mail"
+          value={email}
+          disabled={creatingUser}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="senha"
+          value={password}
+          disabled={creatingUser}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button type="submit" disabled={creatingUser}>
+          Registre-se
+        </button>
         <div className="my-4 text-center text-gray-700">
           Ou faça com sua rede social
         </div>
         <button className="flex items-center justify-center gap-4 btn_secondary">
-        <Image src={'/google.png'} alt={''} width={24} height={24} />
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
           Login com o Google
         </button>
       </form>
